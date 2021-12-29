9f0ccbb1fc549d4fef5489037a02438e3f81486d
EvgeniyPukanovich <a.epik@mail.ru>
2021-12-29 22:10:54 +0500

    removed password

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-let db_connection = 'mongodb+srv://capybara:XPaVr8zn9o0b9FAo@cluster0.fqwva.mongodb.net/local_pub?retryWrites=true&w=majority';
+let db_connection = 'mongodb+srv://capybara:@cluster0.fqwva.mongodb.net/local_pub?retryWrites=true&w=majority';
 module.exports = db_connection
